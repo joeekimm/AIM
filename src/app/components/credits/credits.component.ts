870607627f6a2f110703db85743322ac507440a0
Yubin Kim <joeekimm@gmail.com>
2019-08-29 09:09:52 -0400

    modified credits component to include logic for parameterizing an array of album pictures into a list of cards; added styling

--- src/app/components/credits/credits.component.ts
+++ src/app/components/credits/credits.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PicList } from '../../../assets/pic-list';
+
 @Component({
   selector: 'app-credits',
   templateUrl: './credits.component.html',
   styleUrls: ['./credits.component.sass']
 })
 export class CreditsComponent implements OnInit {
+  pictures;
 
   constructor() { }
 
   ngOnInit() {
+    this.pictures = PicList;
+    console.log(this.pictures);
   }
 
 }

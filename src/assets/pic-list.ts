b32c0fa7fe222ff1085fbfa3f1985bde332c3bbd
Yubin Kim <joeekimm@gmail.com>
2019-08-29 08:59:16 -0400

    added option to change theme colors

---
+++ src/assets/pic-list.ts
@@ -0,0 +1,97 @@
+const PicList = [
+  {
+    imageUrl: 'assets/pictures/8c4ed4df0f373b4db255509b63ae1a13.1000x1000x1.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/298c70a6d741d08e03488dbd7a2ecec4.1000x1000x1.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/535d5510c6f3795144756d8151a068bf.1000x1000x1.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/814IyUai8FL._SL1500_.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/1111+reset.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/2016JamesBlakeThecolourinanything-1-920x920.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/2439289c1da52855a8942add1a446ef0.1000x1000x1.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/1524860530_574e702a20664bae2e4b52399d6e94f6.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/1563949179_d4c199017842fa338fe347134a617098.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/21817895686aa89107978c566e5ff2d4.960x960x1.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/atb.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/big-sean-overtime.jpeg'
+  },
+  {
+    imageUrl: 'assets/pictures/disp3.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/djkhaled_majorkey_5x5_pa_lr_1024-570x570.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/download.jpeg'
+  },
+  {
+    imageUrl: 'assets/pictures/download+(1).jpeg'
+  },
+  {
+    imageUrl: 'assets/pictures/efae5867d6e39372f9815e5f28cde6a5.1000x1000x1.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/fam+not+a+group.jpeg'
+  },
+  {
+    imageUrl: 'assets/pictures/heartbrake.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/icy.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/ktown.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/MJB-asap-rocky-love-yourself-video.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/noone+ever+really+dies.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/plo+shine.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/racks+in+the+middle.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/stoney.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/strength+of+a+woman.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/sweetener.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/tpab.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/yn2.jpg'
+  },
+  {
+    imageUrl: 'assets/pictures/zhavia_17-ep-artwork.jpg'
+  },
+]
+
+export { PicList };
